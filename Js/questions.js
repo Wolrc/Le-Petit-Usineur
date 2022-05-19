1a3d76c14278757632ee286d162de4b9eed53872
Wolrc <wolrcsix@gmail.com>
2022-05-20 00:02:15 +0200

    ajout des dossier

---
+++ Js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Quelle machine utilise l'opérateur au début de la vidéo?",
+        answer: "Un tour à commande numérique",
+        options: [
+            "Un tour à commande numérique",
+            "Un tour traditionnel",
+            "Une Fraiseuse",
+            "Une Perceuse"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Qu'est ce que la P.O.M?",
+        answer: "Prise d'origine Machine",
+        options: [
+            "Prise d'origine Outils",
+            "Prise d'origine Machine",
+            "Prise d'origine Programme",
+            "Un Fruit"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Qu'est ce que le nom du Porte-Outil d'une Fraiseuse?",
+        answer: "Une broche",
+        options: [
+            "Une broche",
+            "Un mandrin",
+            "Un étau",
+            "Une Fraise"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Avec quelle machine d'usinage on usine des pièces cylindriques?",
+        answer: "Un Tour",
+        options: [
+            "Un Tour",
+            "Une Fraiseuse",
+            "Une Perceuse",
+            "Une Cynlindreuse"
+        ]
+    },
+    {
+        numb: 5,
+        question: "A quoi sert le clavier et l'écran sur les machine?",
+        answer: "Utiliser la machine",
+        options: [
+            "Surfer sur le WEB",
+            "Utiliser la machine",
+            "Controler la tension de la machine",
+            "A jouer"
+        ]
+    },
+]
